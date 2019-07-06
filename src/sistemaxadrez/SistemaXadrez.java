2d5a09721b01ae4e073bea8ea26fcd0ebfacd299
ViniciusNogueiraS <v.nog@outlook.com>
2019-07-06 11:47:56 -0300

    classe tabuleiro criada

--- src/sistemaxadrez/SistemaXadrez.java
+++ src/sistemaxadrez/SistemaXadrez.java
@@ -1,9 +1,12 @@
 package sistemaxadrez;
 
+import tabuleiro.Posicao;
+
 public class SistemaXadrez {
 
     public static void main(String[] args) {
-        
+        Posicao p1 = new Posicao(3, 5);
+        System.out.print(p1);
     }
     
 }
